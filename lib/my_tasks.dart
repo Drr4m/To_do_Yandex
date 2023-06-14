35b2bafd0e4b6a907f52a5031fc4a6d1921fc09c
unknown <karenavetisyan.200129@gmail.com>
2023-06-15 00:49:43 +0300

    Added Card in SliverList

--- lib/my_tasks.dart
+++ lib/my_tasks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 import 'package:todoyandex/core/themes/my_dismissable_widget.dart';
 import 'package:todoyandex/my_sliver_bar.dart';
 import 'package:todoyandex/task.dart';
@@ -56,50 +57,65 @@ class _MyTasksState extends State<MyTasks> {
       body: CustomScrollView(
         slivers: <Widget>[
           const MySliverBar(),
-          Container(
-            child: SliverList(
-                delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Dismissible(
-                  key: UniqueKey(),
-                  onDismissed: (direction) {
-                    swipe(index, direction);
-                  },
-                  background: SwipeActionLeft(),
-                  secondaryBackground: SwipeActionRight(),
-                  child: CheckboxListTile(
-                    activeColor: const Color.fromRGBO(52, 199, 89, 1),
-                    value: tasks[index].checked,
-                    secondary: const Icon(Icons.info_outline),
-                    controlAffinity: ListTileControlAffinity.leading,
-                    title: tasks[index].checked == true
-                        ? Text(
-                            tasks[index].title,
-                            style: const TextStyle(
-                                decoration: TextDecoration.lineThrough,
-                                color: Color.fromRGBO(0, 0, 0, 0.3),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400),
-                          )
-                        : Text(
-                            tasks[index].title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          tasks[index].checked = !tasks[index].checked;
-                        },
-                      );
-                    },
-                  ),
-                );
-              },
-              childCount: tasks.length,
-            )),
+          SliverStack(
+            insetOnOverlap: false,
+            children: [
+              const SliverPositioned.fill(
+                  child: Card(
+                elevation: 3,
+              )),
+              SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(4, 4, 4, 8),
+                    child: Dismissible(
+                      key: UniqueKey(),
+                      onDismissed: (direction) {
+                        swipe(index, direction);
+                      },
+                      background: SwipeActionLeft(),
+                      secondaryBackground: SwipeActionRight(),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: CheckboxListTile(
+                          activeColor: const Color.fromRGBO(52, 199, 89, 1),
+                          value: tasks[index].checked,
+                          secondary: const Icon(Icons.info_outline),
+                          controlAffinity: ListTileControlAffinity.leading,
+                          title: tasks[index].checked == true
+                              ? Text(
+                                  tasks[index].title,
+                                  style: const TextStyle(
+                                      decoration: TextDecoration.lineThrough,
+                                      color: Color.fromRGBO(0, 0, 0, 0.3),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                )
+                              : Text(
+                                  tasks[index].title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                          onChanged: (value) {
+                            setState(
+                              () {
+                                tasks[index].checked = !tasks[index].checked;
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                childCount: tasks.length,
+              )),
+            ],
           ),
         ],
       ),
@@ -120,6 +136,7 @@ class _MyTasksState extends State<MyTasks> {
 }
 
 Widget SwipeActionLeft() => Container(
+      margin: EdgeInsets.fromLTRB(4, 0, 4, 0),
       alignment: Alignment.centerLeft,
       padding: const EdgeInsets.fromLTRB(24, 0, 0, 0),
       color: const Color.fromRGBO(52, 199, 89, 1),
@@ -130,6 +147,7 @@ Widget SwipeActionLeft() => Container(
       ),
     );
 Widget SwipeActionRight() => Container(
+      margin: EdgeInsets.fromLTRB(4, 0, 4, 0),
       alignment: Alignment.centerRight,
       padding: const EdgeInsets.fromLTRB(0, 0, 24, 0),
       color: const Color.fromRGBO(255, 59, 48, 1),
